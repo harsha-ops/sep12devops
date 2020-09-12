1b58f1d5430bdce5d9a46dfc98bd64bd1fce244e
root <root@ip-10-1-1-229.eu-west-2.compute.internal>
2020-09-12 07:45:49 +0000

    Added echo to the Script

--- userscript.sh
+++ userscript.sh
@@ -3,4 +3,4 @@ echo "This Script Create Users..!"
 read -p "Please Enter the Username:" Username
 echo $Username
 read -s -p "Please Enter the Password:" Password
-
+echo "User created Successfully..!"
